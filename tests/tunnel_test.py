00041f6cb7c6b9098f9b1c6cfadda21db0bf2c2a
devarogundade <devarogundade@gmail.com>
2023-10-27 20:49:56 +0100

    Project initialised with AlgoKit CLI using template: https://github.com/algorandfoundation/algokit-beaker-default-template.git

---
+++ tests/tunnel_test.py
@@ -0,0 +1,33 @@
+import pytest
+from algokit_utils import (
+    ApplicationClient,
+    ApplicationSpecification,
+    get_localnet_default_account,
+)
+from algosdk.v2client.algod import AlgodClient
+
+from smart_contracts.tunnel import contract as tunnel_contract
+
+
+@pytest.fixture(scope="session")
+def tunnel_app_spec(algod_client: AlgodClient) -> ApplicationSpecification:
+    return tunnel_contract.app.build(algod_client)
+
+
+@pytest.fixture(scope="session")
+def tunnel_client(
+    algod_client: AlgodClient, tunnel_app_spec: ApplicationSpecification
+) -> ApplicationClient:
+    client = ApplicationClient(
+        algod_client,
+        app_spec=tunnel_app_spec,
+        signer=get_localnet_default_account(algod_client),
+    )
+    client.create()
+    return client
+
+
+def test_says_hello(tunnel_client: ApplicationClient) -> None:
+    result = tunnel_client.call(tunnel_contract.hello, name="World")
+
+    assert result.return_value == "Hello, World"
