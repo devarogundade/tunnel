71dff270e7a018b57e8e747725bea620282fecde
devarogundade <devarogundade@gmail.com>
2023-11-06 09:11:36 +0100

    fix

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   define: {
-    global: {},
+    global: 'window',
     'process.env': {},
   },
   optimizeDeps: {
