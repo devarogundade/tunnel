03e9a077b27041a73285a1ae3b13cdca3a362108
devarogundade <devarogundade@gmail.com>
2023-11-06 08:38:13 +0100

    update:

--- vue/src/scripts/bridge.js
+++ vue/src/scripts/bridge.js
@@ -37,33 +37,18 @@ const wormholeAcct = 'C2SZBD4ZFFDXANBCUTG5GBUEWMQ34JS5LFGDRTEVJBAXDRF6ZWB7Q4KHHM
 const storageAcct = 'PBWLEPGMMT6JFNWSVCIE5SJH5YAVNVY3AWOFABQDH4XQWA3RCNVKK3TXYY'
 
 const algodClient = algokit.getAlgoClient({
-    server: 'https://testnet-api.algonode.cloud'
+    server: 'https://testnet-api.algonode.cloud',
+    token: '',
+    port: ''
 });
 
 const peraWallet = new PeraWalletConnect({
     chainId: 416002,
 });
 
-
-const methods = [
-    new algosdk.ABIMethod({ name: "opt_in_asset", desc: "", args: [{ type: "asset", name: "asset", desc: "" }], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "set_collateral", desc: "", args: [{ type: "byte", name: "name", desc: "" }, { type: "byte", name: "symbol", desc: "" }, { type: "uint64", name: "supply", desc: "" }], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "supply", desc: "", args: [{ type: "pay", name: "payment", desc: "" }], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "borrow", desc: "", args: [{ type: "asset", name: "asset", desc: "" }], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "repay", desc: "", args: [{ type: "pay", name: "payment", desc: "" }], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "withdraw", desc: "", args: [], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "receiveMessage", desc: "", args: [{ type: "uint64", name: "nonce", desc: "" }, { type: "asset", name: "asset", desc: "" }, { type: "uint64", name: "amount", desc: "" }, { type: "account", name: "receiver", desc: "" }], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "un_bridge", desc: "", args: [{ type: "asset", name: "asset", desc: "" }, { type: "application", name: "wormhole", desc: "" }, { type: "account", name: "wormhole_account", desc: "" }, { type: "account", name: "storage_account", desc: "" }], returns: { type: "void", desc: "" } }),
-    new algosdk.ABIMethod({ name: "borrow_of", desc: "", args: [{ type: "address", name: "address", desc: "" }], returns: { type: "(uint64,uint64,uint64)", desc: "" } }),
-    new algosdk.ABIMethod({ name: "supply_of", desc: "", args: [{ type: "address", name: "address", desc: "" }], returns: { type: "(uint64,uint64)", desc: "" } })
-]
+console.log(await algodClient.getApplicationByID(tunnelId).do());
 
 const account = algokit.mnemonicAccount("skin embody only fit fine fit identify refuse bench enroll dentist million axis luxury train liquid kitten stumble layer fall airport deliver laugh ability ill")
-
-const signer = (trxs) => {
-    console.log(trxs);
-    return algosdk.signTransaction(trxs, account.sk)
-}
 
 export async function trySupply(amount) {
     try {
@@ -71,6 +56,8 @@ export async function trySupply(amount) {
 
 
         // if (accounts.length == 0) return null
+
+        console.log(account);
 
         const suggestedParams = await algodClient.getTransactionParams().do();
 
@@ -86,10 +73,8 @@ export async function trySupply(amount) {
             from: account.addr,
             foreignApps: [tunnelId],
             boxes: [{ appIndex: tunnelId, name: algosdk.decodeAddress(account.addr).publicKey }],
-            appArgs: [
-                algosdk.encodeObj('supply'),
-            ],
-            suggestedParams,
+            appArgs: [algosdk.encodeObj('supply')],
+            suggestedParams
         })
 
         algosdk.assignGroupID([payment, appCall])
